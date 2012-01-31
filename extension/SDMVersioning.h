947c3636c2a3d5ce5a2ce5b41b502b7e3a05e4d6
Dustin L. Howett <dustin@howett.net>
2012-01-30 23:31:44 -0800

    Version checking macros!

---
+++ extension/SDMVersioning.h
@@ -0,0 +1,16 @@
+#define _SDM_iOS_2_0 kCFCoreFoundationVersionNumber_iPhoneOS_2_0
+#define _SDM_iOS_2_1 kCFCoreFoundationVersionNumber_iPhoneOS_2_1
+#define _SDM_iOS_2_2 kCFCoreFoundationVersionNumber_iPhoneOS_2_2
+#define _SDM_iOS_3_0 kCFCoreFoundationVersionNumber_iPhoneOS_3_0
+#define _SDM_iOS_3_1 kCFCoreFoundationVersionNumber_iPhoneOS_3_1
+#define _SDM_iOS_3_2 kCFCoreFoundationVersionNumber_iPhoneOS_3_2
+#define _SDM_iOS_4_0 kCFCoreFoundationVersionNumber_iOS_4_0
+#define _SDM_iOS_4_1 kCFCoreFoundationVersionNumber_iOS_4_1
+#define _SDM_iOS_4_2 kCFCoreFoundationVersionNumber_iOS_4_2
+#define _SDM_iOS_5_0 675.00
+
+#define SDMSystemVersionEQ(v) (kCFCoreFoundationVersionNumber == v)
+#define SDMSystemVersionLT(v) (kCFCoreFoundationVersionNumber <  v)
+#define SDMSystemVersionLE(v) (kCFCoreFoundationVersionNumber <= v)
+#define SDMSystemVersionGT(v) (kCFCoreFoundationVersionNumber >  v)
+#define SDMSystemVersionGE(v) (kCFCoreFoundationVersionNumber >= v)
